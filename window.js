11ddf0c973e9fc05cf30bc704ae8c65d9592b0a4
paulv160 <75595723+paulv160@users.noreply.github.com>
2021-11-22 13:36:11 -0500

    filetype

--- window.js
+++ window.js
@@ -3,47 +3,79 @@ const path = require('path')
 const fs = require('fs')
 const os = require('os')
 
-let DEFAULT_PATH = '/Users/paul/Programming'
+const DEFAULT_PATH = '/Users/paul/Programming'
 let CURR_PATH
 
+const get_file_table = (files) => {
+  return `
+  <table id='filename-list'>
+    <thead>
+      <tr>
+        <th id='filename-header'>Files in ${CURR_PATH}</th>
+        <th>Type</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${
+        Array.from(files).map(
+          file => get_file_elem(file)
+        ).join('\n')
+      }
+    </tbody>
+  </table>
+  `
+}
+
+const get_file_type = (filename) => {
+  if (fs.lstatSync(filename).isDirectory()) 
+    return 'Folder'
+  return {
+    txt: 'Text File',
+    js: 'JavaScript Source'
+  }
+}
+
 const get_file_elem = (filename) => {
-  return `<li>${filename}</li>`
+  return `
+  <tr>
+    <td>
+      ${filename}
+    </td>
+    <td>
+      ${path.extname(filename)}
+    </td>
+  </tr>`
 }
 
 const change_path = (newpath) => {
   CURR_PATH = newpath
-  $('#filename-header').html(`Current Directory: ${newpath}`)
-  $('#filename-list').empty()
+  $('#filename-container').empty()
   fs.readdir(
     newpath, (err, files) => {
       if (err)
         console.log(err)
       else {
-        files.forEach(file => {
-          let elem = get_file_elem(file)
-          $('#filename-list').append(elem)
-        })
+        $('#filename-container').html(
+          get_file_table(files)
+        )
       }
     }
   )
 }
 
 
-/* on ready */
-$(() => {
+$(document).ready(
+  () => {
 
-  $('#changedir-submit').click(
-    () => {
-    change_path(
-      $('#changedir-input').val()
-    )
-    console.log('a')
+    $('#changedir-submit').on('click',
+      () => {
+        change_path(
+          $('#changedir-input').val()
+        )
+    })
 
+    change_path(DEFAULT_PATH)
+
+    console.log('Ready')
   }
 )
-
-
-  change_path(DEFAULT_PATH)
-  console.log('Ready')
-  console.log($('#changedir-submit'))
-})
